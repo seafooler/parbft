c74615567fcb83a00f6697051ee8d9f57b104763
seafooler <seafooler@gmail.com>
2023-03-11 18:43:43 -0600

    debug

--- core/node.go
+++ core/node.go
@@ -630,7 +630,7 @@ func (n *Node) WaitForEnoughSyncLaunchMsgs() error {
 			switch msg.(type) {
 			case PaceSyncMsg:
 				count += 1
-				if count >= n.N-3 {
+				if count >= n.N {
 					return nil
 				}
 			default:
